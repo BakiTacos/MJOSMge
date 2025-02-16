b9141600b4dc272866997b6b41d4bcab14a4cc5c
Kevin Sakia Putra <kevinsakia.p@gmail.com>
2025-02-16 23:51:42 +0700

    huas

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBvtvkgzoNWT2gVGl3zLETYPnhKUP-dGc8",
+    authDomain: "mjosmgedb.firebaseapp.com",
+    databaseURL: "https://mjosmgedb-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "mjosmgedb",
+    storageBucket: "mjosmgedb.appspot.com",
+    messagingSenderId: "92899208044",
+    appId: "1:92899208044:web:c765306470956717952149",
+    measurementId: "G-5045ZX790R"
+};
